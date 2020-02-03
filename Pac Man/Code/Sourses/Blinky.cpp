4fdb2ad647cadf1b137fd15b9b75f394ec9a9cac
Veresless <43114078+Veresless@users.noreply.github.com>
2020-02-04 01:06:51 +0200

    Delete Blinky.cpp

--- Pac Man/Code/Sourses/Blinky.cpp
+++
@@ -1,7 +0,0 @@
-#include "../Headers/Blinky.h"
-
-Blinky::Blinky(): Gost(getRed(), getBlue(), getBlinkyStartPosition(), getBlinkyRetreatPoint()){}
-void Blinky::setDirectionPoint(const POINT& point, const Direction direction, const POINT& blinky)
-{
-	directionPoint_ = point;
-}
